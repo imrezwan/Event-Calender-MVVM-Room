fcfd6a4507e047fe7697aa04f9d7c2c3b232ae73
RkPranto <rezwan2525@gmail.com>
2021-09-25 18:58:37 +0600

    Initial commit

---
+++ app/src/main/java/com/rezwan2525/event_calender_mvvm_room/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rezwan2525.event_calender_mvvm_room;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
