05eb915bb1f8276e2e36942fee6baa4641252ff7
RkPranto <rezwan2525@gmail.com>
2021-09-28 22:57:28 +0600

    appbar ui done

--- app/src/main/java/com/rezwan2525/event_calender_mvvm_room/MainActivity.java
+++ app/src/main/java/com/rezwan2525/event_calender_mvvm_room/MainActivity.java
@@ -3,6 +3,7 @@ package com.rezwan2525.event_calender_mvvm_room;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
